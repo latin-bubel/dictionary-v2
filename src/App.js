573f265dd31cf771daa655f539dfb219226cae00
latin bubel <esterapietkiewicz0110@gmail.com>
2023-07-30 13:12:21 +0200

    Integrated dictionary API
    https://dictionaryapi.dev/

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Dictionary</header>
+      <div className="container">
+        <header className="App-header">Dictionary</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Estera Pietkiewicz</small>
+        </footer>
+      </div>
     </div>
   );
 }
-
 export default App;
