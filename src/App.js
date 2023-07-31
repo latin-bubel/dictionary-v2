46e1337952e77032ec9719f6bf6b127249fdabf1
latin bubel <esterapietkiewicz0110@gmail.com>
2023-07-31 12:48:33 +0200

    Included photos component and added credits

--- src/App.js
+++ src/App.js
@@ -10,7 +10,37 @@ function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by Estera Pietkiewicz</small>
+          <small>
+            {" "}
+            <a
+              href="https://github.com/latin-bubel/dictionary-v2"
+              title="Github repository"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>
+            &nbsp;by&nbsp;
+            <a
+              href="https://pl.linkedin.com/in/estera-pietkiewicz-6b7558178?original_referer=https%3A%2F%2Fwww.google.com%2F"
+              target="_blank"
+              rel="noreferrer"
+              title="Estera - Linkedin"
+            >
+              Estera Pietkiewicz
+            </a>{" "}
+            <br />
+            ✨Hosted on&nbsp;Netlify
+            <br />
+            <a
+              href="https://www.pexels.com "
+              target="_blank"
+              rel="noreferrer"
+              title="Pexels"
+            >
+              Photos provided by Pexels
+            </a>
+          </small>
         </footer>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    //documentation = https://www.pexels.com/api/documentation/#photos-search
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    const pexelsApiKey =
+      "sd02j0fYRhRkLIZOCc8Cge3w3lAMz0WC719gYf9K6EOc7FblOXbfn2we";
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -49,6 +63,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
